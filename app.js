2856efc4aea014a53a5dea7213b06ef5730218d0
jinseo-jang <jinseo.jang@gmail.com>
2020-08-30 23:56:32 +0900

    routing module added

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var mysql = require("mysql");
-var db_config = require("./config/db-config.json");
-
-var connection = mysql.createConnection({
-  host: db_config.host,
-  port: db_config.port,
-  user: db_config.user,
-  password: db_config.password,
-  database: db_config.database,
-});
-
-connection.connect();
+var main = require("./router/main");
+var email = require("./router/email");
 
 app.listen(3000, function () {
   console.log("start@ express server on port 3000!");
@@ -25,35 +15,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+app.use("/main", main);
+app.use("/email", email);
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/main.html");
-});
-
-app.get("/main", function (req, res) {
-  res.sendFile(__dirname + "/public/main.html");
-});
-
-app.post("/email_post", function (req, res) {
-  console.log(req.body.email);
-  //to convert ejs to html, use render function
-  res.render("email.ejs", { email: req.body.email });
-});
-
-app.post("/ajax_send_email", function (req, res) {
-  var email = req.body.email;
-  var responseData = {};
-
-  var query = connection.query(
-    'select name from user where email="' + email + '"',
-    function (err, rows) {
-      if (err) throw err;
-      if (rows[0]) {
-        console.log(rows[0]);
-      } else {
-        console.log("none : " + rows[0]);
-      }
-    }
-  );
 });
 
 // app.post("/search", function (req, res) {
---
+++ router/email.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = require("path");
+
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+var db_config = require("../config/db-config.json");
+
+var connection = mysql.createConnection({
+  host: db_config.host,
+  port: db_config.port,
+  user: db_config.user,
+  password: db_config.password,
+  database: db_config.database,
+});
+connection.connect();
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+router.post("/form", function (req, res) {
+  console.log(req.body.email);
+  //to convert ejs to html, use render function
+  res.render("email.ejs", { email: req.body.email });
+});
+
+router.post("/ajax", function (req, res) {
+  var email = req.body.email;
+  var responseData = {};
+
+  var query = connection.query(
+    'select name from user where email="' + email + '"',
+    function (err, rows) {
+      if (err) throw err;
+      if (rows[0]) {
+        responseData.result = "ok";
+        responseData.name = rows[0].name;
+        console.log(rows[0]);
+      } else {
+        responseData.result = "none";
+        responseData.name = "";
+        console.log("none : " + rows[0]);
+      }
+      res.json(responseData);
+    }
+  );
+});
+
+module.exports = router;
